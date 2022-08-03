fb8d7cd47560bff0fe215da34df4e37a1163e51d
David <David@RYZENIUS>
2022-08-03 16:53:54 +0200

    Add project files.

---
+++ DemoLibrary/PersonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class PersonModel
+    {
+        private string firstname;
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        private string lastname;
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+}
---
+++ SQLiteDemo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DemoLibrary;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace SQLiteDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<PersonModel> people = new List<PersonModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadPeopleList()
+        {
+            people = SqliteDataAccess.LoadPeople();
+
+            WireUpPeopleList();
+        }
+
+        private void WireUpPeopleList()
+        {
+            listPeopleListBox.ItemsSource = null;
+            listPeopleListBox.Items.Clear();
+            listPeopleListBox.ItemsSource = people;
+        }
+
+        private void addPersonBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel p = new PersonModel();
+
+            p.Firstname = firstNameText.Text;
+            p.Lastname = lastNameText.Text;
+
+            SqliteDataAccess.SavePerson(p);
+            WireUpPeopleList();
+
+            //reset textbox content
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+        }
+
+        private void refreshListBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPeopleList();
+        }
+    }
+}
